7da6406ff9136f01b2c8960a1f13193074a55a6d
Reynaldo R. Martinez P <tigerlinux@gmail.com>
2015-11-17 19:17:36 -0430

    0.9.0.el7 "Nuclear Lynx"
    - Added ALERT-AND-BUGS documentation to the installer
      regarding Domains usage with keystone V3 and some
      OpenStack bugs with postgres affecting cinder and
      trove.
    - Added Keystone V3 support to Horizon.
    - Included some ALERTS in the main config file.
    - Added the capability to provision VLAN external net-
      works along the FLAT external ones.
    - All modules converted to LIBERTY from former installer
      made for Kilo.
    - Fully converted to Identity V3.
    - New options for FLAT networks configuration (neutron
      install module).

---
+++ modules/ceilometerinstall.sh
@@ -0,0 +1,445 @@
+#!/bin/bash
+#
+# Unattended/SemiAutomatted OpenStack Installer
+# Reynaldo R. Martinez P.
+# E-Mail: TigerLinux@Gmail.com
+# OpenStack LIBERTY for Centos 7
+#
+#
+
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+
+#
+# First, we source our config file and verify that some important proccess are 
+# already completed.
+#
+
+if [ -f ./configs/main-config.rc ]
+then
+	source ./configs/main-config.rc
+	mkdir -p /etc/openstack-control-script-config
+else
+	echo "Can't access my configuration file. Aborting"
+	echo ""
+	exit 0
+fi
+
+if [ -f /etc/openstack-control-script-config/db-installed ]
+then
+	echo ""
+	echo "Database Process OK. Let's continue"
+	echo ""
+else
+	echo ""
+	echo "Database Process not completed. Aborting"
+	echo ""
+	exit 0
+fi
+
+if [ -f /etc/openstack-control-script-config/keystone-installed ]
+then
+	echo ""
+	echo "Keystone Process OK. Let's continue"
+	echo ""
+else
+	echo ""
+	echo "Keystone Process not completed. Aborting"
+	echo ""
+	exit 0
+fi
+
+if [ -f /etc/openstack-control-script-config/ceilometer-installed ]
+then
+	echo ""
+	echo "This module was already completed. Exiting"
+	echo ""
+	exit 0
+fi
+
+#
+# Here, depending if we want to install a ceilometer controller or a ceilometer
+# in a compute node, we install the proper packages for the selection
+#
+
+echo ""
+echo "Installing Ceilometer Packages"
+
+if [ $ceilometer_in_compute_node == "no" ]
+then
+	echo ""
+	echo "ALL-IN-ONE or Controller Packages"
+	echo ""
+	yum install -y openstack-ceilometer-api \
+		openstack-ceilometer-central \
+		openstack-ceilometer-collector \
+		openstack-ceilometer-common \
+		openstack-ceilometer-compute \
+		python-ceilometerclient \
+		python-ceilometer \
+		python-ceilometerclient-doc \
+		openstack-utils \
+		openstack-selinux
+
+	if [ $ceilometeralarms == "yes" ]
+	then
+		yum install -y openstack-ceilometer-alarm
+	fi
+else
+	echo ""
+	echo "Compute Node Packages"
+	echo ""
+	yum install -y openstack-utils \
+		openstack-selinux \
+		openstack-ceilometer-compute \
+		python-ceilometerclient \
+		python-pecan
+fi
+
+#
+# FIX for ceilometer missing awsauth module - not currently on EPEL or RDO repos
+# We proceed to install it using pip
+#
+yum -y install python-pip
+pip install requests-aws
+
+echo "Listo"
+echo ""
+
+source $keystone_admin_rc_file
+
+#
+# We install and configure Mongo DB, but ONLY if this is not a compute node
+#
+
+if [ $ceilometer_in_compute_node = "no" ]
+then
+	echo ""
+	echo "Installing and Configuring MongoDB Database Backend"
+	echo ""
+
+	yum -y install mongodb-server mongodb
+	sed -i '/--smallfiles/!s/OPTIONS=\"/OPTIONS=\"--smallfiles /' /etc/sysconfig/mongod
+	sed -i "s/127.0.0.1/$mondbhost/g" /etc/mongod.conf
+	sed -i "s/27017/$mondbport/g" /etc/mongod.conf
+
+	service mongod start
+	chkconfig mongod on
+
+	mongo --host $mondbhost --eval "db = db.getSiblingDB(\"$mondbname\");db.createUser({user: \"$mondbuser\",pwd: \"$mondbpass\",roles: [ \"readWrite\", \"dbAdmin\" ]})"
+
+	sync
+	sleep 5
+	sync
+fi
+
+echo ""
+echo "Configuring Ceilometer"
+echo ""
+
+#
+# Using python based tools, we proceed to configure ceilometer
+#
+
+echo "#" >> /etc/ceilometer/ceilometer.conf
+
+#
+# Using python based tools, we proceed to configure ceilometer
+#
+ 
+# Deprecated !
+#crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken admin_tenant_name $keystoneservicestenant
+#crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken admin_user $ceilometeruser
+#crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken admin_password $ceilometerpass
+#crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken auth_uri http://$keystonehost:5000/v2.0
+#crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken identity_uri http://$keystonehost:35357
+ 
+# New settings for LIBERTY
+crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken auth_plugin password
+crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken auth_section keystone_authtoken
+crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken username $ceilometeruser
+crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken password $ceilometerpass
+crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken project_domain_id $keystonedomain
+crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken user_domain_id $keystonedomain
+crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken project_name $keystoneservicestenant
+crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken auth_uri http://$keystonehost:5000/v2.0
+crudini --set /etc/ceilometer/ceilometer.conf keystone_authtoken auth_url http://$keystonehost:35357
+ 
+ 
+# Deprecated !
+# crudini --set /etc/ceilometer/ceilometer.conf DEFAULT os_auth_url "http://$keystonehost:35357/v2.0"
+# crudini --set /etc/ceilometer/ceilometer.conf DEFAULT os_tenant_name $keystoneservicestenant
+# crudini --set /etc/ceilometer/ceilometer.conf DEFAULT os_password $ceilometerpass
+# crudini --set /etc/ceilometer/ceilometer.conf DEFAULT os_username $ceilometeruser
+# crudini --set /etc/ceilometer/ceilometer.conf DEFAULT os_auth_region $endpointsregion
+ 
+crudini --set /etc/ceilometer/ceilometer.conf service_credentials os_username $ceilometeruser
+crudini --set /etc/ceilometer/ceilometer.conf service_credentials os_password $ceilometerpass
+crudini --set /etc/ceilometer/ceilometer.conf service_credentials os_tenant_name $keystoneservicestenant
+crudini --set /etc/ceilometer/ceilometer.conf service_credentials os_auth_url http://$keystonehost:5000/v2.0
+crudini --set /etc/ceilometer/ceilometer.conf service_credentials os_region_name $endpointsregion
+crudini --set /etc/ceilometer/ceilometer.conf service_credentials os_endpoint_type internalURL
+ 
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT metering_api_port 8777
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT auth_strategy keystone
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT log_dir /var/log/ceilometer
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT os_auth_region $endpointsregion
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT host `hostname`
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT pipeline_cfg_file pipeline.yaml
+crudini --set /etc/ceilometer/ceilometer.conf collector workers 2
+crudini --set /etc/ceilometer/ceilometer.conf notification workers 2
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT hypervisor_inspector libvirt
+ 
+crudini --del /etc/ceilometer/ceilometer.conf DEFAULT sql_connection > /dev/null 2>&1
+crudini --del /etc/ceilometer/ceilometer.conf DEFAULT sql_connection > /dev/null 2>&1
+ 
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT nova_control_exchange nova
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT glance_control_exchange glance
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT neutron_control_exchange neutron
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT cinder_control_exchange cinder
+ 
+crudini --set /etc/ceilometer/ceilometer.conf publisher telemetry_secret $metering_secret
+
+kvm_possible=`grep -E 'svm|vmx' /proc/cpuinfo|uniq|wc -l`
+if [ $kvm_possible == "0" ]
+then
+	crudini --set /etc/ceilometer/ceilometer.conf DEFAULT libvirt_type qemu
+else
+	crudini --set /etc/ceilometer/ceilometer.conf DEFAULT libvirt_type kvm
+fi
+
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT debug false
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT verbose false
+crudini --set /etc/ceilometer/ceilometer.conf database connection "mongodb://$mondbuser:$mondbpass@$mondbhost:$mondbport/$mondbname"
+crudini --set /etc/ceilometer/ceilometer.conf database metering_time_to_live $mongodbttl
+crudini --set /etc/ceilometer/ceilometer.conf database time_to_live $mongodbttl
+# Deprecated
+# crudini --set /etc/ceilometer/ceilometer.conf rpc_notifier2 topics notifications,glance_notifications
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT notification_topics notifications,glance_notifications
+# crudini --set /etc/ceilometer/ceilometer.conf DEFAULT policy_file policy.json
+# crudini --set /etc/ceilometer/ceilometer.conf DEFAULT policy_default_rule default
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT dispatcher database
+ 
+case $brokerflavor in
+"qpid")
+	crudini --set /etc/ceilometer/ceilometer.conf DEFAULT rpc_backend qpid
+	crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_qpid qpid_hostname $messagebrokerhost
+	crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_qpid qpid_port 5672
+	crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_qpid qpid_username $brokeruser
+	crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_qpid qpid_password $brokerpass
+	crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_qpid qpid_heartbeat 60
+	crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_qpid qpid_protocol tcp
+	crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_qpid qpid_tcp_nodelay True
+	;;
+ 
+"rabbitmq")
+	crudini --set /etc/ceilometer/ceilometer.conf DEFAULT rpc_backend rabbit
+	crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_rabbit rabbit_host $messagebrokerhost
+	crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_rabbit rabbit_password $brokerpass
+	crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_rabbit rabbit_userid $brokeruser
+	crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_rabbit rabbit_port 5672
+	crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_rabbit rabbit_use_ssl false
+	crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_rabbit rabbit_virtual_host $brokervhost
+	crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_rabbit rabbit_max_retries 0
+	crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_rabbit rabbit_retry_interval 1
+	crudini --set /etc/ceilometer/ceilometer.conf oslo_messaging_rabbit rabbit_ha_queues false
+	;;
+esac
+
+ 
+crudini --set /etc/ceilometer/ceilometer.conf alarm evaluation_service ceilometer.alarm.service.SingletonAlarmService
+crudini --set /etc/ceilometer/ceilometer.conf alarm partition_rpc_topic alarm_partition_coordination
+# crudini --set /etc/ceilometer/ceilometer.conf alarm evaluation_interval 60
+# crudini --set /etc/ceilometer/ceilometer.conf alarm record_history True
+crudini --set /etc/ceilometer/ceilometer.conf api port 8777
+crudini --set /etc/ceilometer/ceilometer.conf api host 0.0.0.0
+ 
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT heat_control_exchange heat
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT control_exchange ceilometer
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT http_control_exchanges nova
+sed -r -i 's/http_control_exchanges\ =\ nova/http_control_exchanges=nova\nhttp_control_exchanges=glance\nhttp_control_exchanges=cinder\nhttp_control_exchanges=neutron\n/' /etc/ceilometer/ceilometer.conf
+crudini --set /etc/ceilometer/ceilometer.conf publisher_rpc metering_topic metering
+ 
+crudini --set /etc/ceilometer/ceilometer.conf DEFAULT instance_name_template $instance_name_template
+crudini --set /etc/ceilometer/ceilometer.conf service_types neutron network
+crudini --set /etc/ceilometer/ceilometer.conf service_types nova compute
+crudini --set /etc/ceilometer/ceilometer.conf service_types swift object-store
+crudini --set /etc/ceilometer/ceilometer.conf service_types glance image
+crudini --del /etc/ceilometer/ceilometer.conf service_types kwapi
+
+#
+# If this is NOT a compute node, and we are installing swift, then we reconfigure it
+# so it can report to ceilometer too
+#
+
+if [ $ceilometer_in_compute_node == "no" ]
+then
+	if [ $swiftinstall == "yes" ]
+	then
+		yum -y install python-ceilometermiddleware
+                # crudini --set /etc/swift/proxy-server.conf filter:ceilometer use "egg:ceilometer#swift"
+                crudini --set /etc/swift/proxy-server.conf filter:keystoneauth operator_roles "$keystoneadmintenant,$keystoneuserrole,$keystonereselleradminrole"
+                crudini --set /etc/swift/proxy-server.conf pipeline:main pipeline "catch_errors gatekeeper healthcheck proxy-logging cache container_sync bulk ratelimit authtoken keystoneauth container-quotas account-quotas slo dlo versioned_writes proxy-logging ceilometer proxy-server"
+                crudini --set /etc/swift/proxy-server.conf filter:ceilometer paste.filter_factory ceilometermiddleware.swift:filter_factory
+                crudini --set /etc/swift/proxy-server.conf filter:ceilometer control_exchange swift
+                crudini --set /etc/swift/proxy-server.conf filter:ceilometer driver messagingv2
+                crudini --set /etc/swift/proxy-server.conf filter:ceilometer topic notifications
+                crudini --set /etc/swift/proxy-server.conf filter:ceilometer log_level WARN
+                case $brokerflavor in
+                "qpid")
+                        crudini --set /etc/swift/proxy-server.conf filter:ceilometer url qpid://$brokeruser:$brokerpass@$messagebrokerhost:5672/
+                        ;;
+                "rabbitmq")
+                        crudini --set /etc/swift/proxy-server.conf filter:ceilometer url rabbit://$brokeruser:$brokerpass@$messagebrokerhost:5672/$brokervhost
+                        ;;
+                esac
+	        touch /var/log/ceilometer/swift-proxy-server.log
+	        chown swift.swift /var/log/ceilometer/swift-proxy-server.log
+	        usermod -a -G ceilometer swift
+
+		service openstack-swift-proxy stop
+		service openstack-swift-proxy start
+	fi
+fi
+
+#
+# Ceilometer User need to be part of nova and qemu/kvm/libvirt groups
+#
+
+usermod -G qemu,kvm,nova ceilometer > /dev/null 2>&1
+
+#
+# With all configuration done, we proceed to make IPTABLES changes and start ceilometer services
+#
+
+echo ""
+echo "Applying IPTABLES Rules"
+
+iptables -A INPUT -p tcp -m multiport --dports 8777,$mondbport -j ACCEPT
+service iptables save
+
+echo "Ready..."
+
+echo ""
+echo "Cleaning UP App logs"
+ 
+for mylog in `ls /var/log/ceilometer/*.log`; do echo "" > $mylog;done
+ 
+echo "Done"
+echo ""
+
+echo "Starting Ceilometer Services"
+echo ""
+
+if [ $ceilometer_in_compute_node == "no" ]
+then
+	if [ $ceilometer_without_compute == "no" ]
+	then
+		systemctl start openstack-ceilometer-compute
+		systemctl enable openstack-ceilometer-compute
+		# service openstack-ceilometer-compute start
+		# chkconfig openstack-ceilometer-compute on
+	else
+		systemctl stop openstack-ceilometer-compute
+		systemctl disable openstack-ceilometer-compute
+		# service openstack-ceilometer-compute stop
+		# chkconfig openstack-ceilometer-compute off
+	fi
+
+	systemctl start openstack-ceilometer-central
+	systemctl enable openstack-ceilometer-central
+	# service openstack-ceilometer-central start
+	# chkconfig openstack-ceilometer-central on
+
+	systemctl start openstack-ceilometer-api
+	systemctl enable openstack-ceilometer-api
+	# service openstack-ceilometer-api start
+	# chkconfig openstack-ceilometer-api on
+
+	systemctl start openstack-ceilometer-collector
+	systemctl enable openstack-ceilometer-collector
+	# service openstack-ceilometer-collector start
+	# chkconfig openstack-ceilometer-collector on
+
+	systemctl start openstack-ceilometer-notification
+	systemctl enable openstack-ceilometer-notification
+	# service openstack-ceilometer-notification start
+	# chkconfig openstack-ceilometer-notification on
+
+	if [ $ceilometeralarms == "yes" ]
+	then
+		systemctl start openstack-ceilometer-alarm-notifier
+		systemctl enable openstack-ceilometer-alarm-notifier
+		# service openstack-ceilometer-alarm-notifier start
+		# chkconfig openstack-ceilometer-alarm-notifier on
+
+		systemctl start openstack-ceilometer-alarm-evaluator
+		systemctl enable openstack-ceilometer-alarm-evaluator
+		# service openstack-ceilometer-alarm-evaluator start
+		# chkconfig openstack-ceilometer-alarm-evaluator on
+	fi
+
+	cp ./libs/ceilometer-expirer.crontab /etc/cron.d/
+
+	# systemctl stop mongod
+	# systemctl start mongod
+	# service mongod stop
+	# service mongod start
+
+	# service openstack-ceilometer-api restart
+	# service openstack-ceilometer-compute restart
+	# service openstack-ceilometer-central restart
+	# service openstack-ceilometer-collector restart
+	# service openstack-ceilometer-notification restart
+
+	systemctl restart crond
+	# service crond restart
+
+	sync
+	sleep 2
+	sync
+
+else
+	systemctl start openstack-ceilometer-compute
+	systemctl enable openstack-ceilometer-compute
+	# service openstack-ceilometer-compute start
+	# chkconfig openstack-ceilometer-compute on
+	# service openstack-ceilometer-compute restart
+fi
+
+#
+# Finally, we test if our packages are correctly installed, and if not, we set a fail
+# variable that makes the installer to stop further processing
+#
+
+testceilometer=`rpm -qi openstack-ceilometer-compute|grep -ci "is not installed"`
+if [ $testceilometer == "1" ]
+then
+	echo ""
+	echo "Ceilometer Installation FAILED. Aborting !"
+	echo ""
+	exit 0
+else
+	date > /etc/openstack-control-script-config/ceilometer-installed
+	date > /etc/openstack-control-script-config/ceilometer
+	if [ $ceilometeralarms == "yes" ]
+	then
+		date > /etc/openstack-control-script-config/ceilometer-installed-alarms
+	fi
+	if [ $ceilometer_in_compute_node == "no" ]
+	then
+		date > /etc/openstack-control-script-config/ceilometer-full-installed
+	fi
+	if [ $ceilometer_without_compute == "yes" ]
+	then
+		if [ $ceilometer_in_compute_node == "no" ]
+		then
+			date > /etc/openstack-control-script-config/ceilometer-without-compute
+		fi
+	fi
+fi
+
+
+echo ""
+echo "Ceilometer Installed and Configured"
+echo ""
+
---
+++ modules/heatinstall.sh
@@ -0,0 +1,288 @@
+#!/bin/bash
+#
+# Unattended/SemiAutomatted OpenStack Installer
+# Reynaldo R. Martinez P.
+# E-Mail: TigerLinux@Gmail.com
+# OpenStack LIBERTY for Centos 7
+#
+#
+
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+
+#
+# First, we source our config file and verify that some important proccess are 
+# already completed.
+#
+
+if [ -f ./configs/main-config.rc ]
+then
+	source ./configs/main-config.rc
+	mkdir -p /etc/openstack-control-script-config
+else
+	echo "Can't access my config file. Aborting !"
+	echo ""
+	exit 0
+fi
+
+if [ -f /etc/openstack-control-script-config/db-installed ]
+then
+	echo ""
+	echo "DB Proccess OK. Let's continue"
+	echo ""
+else
+	echo ""
+	echo "DB Proccess not completed. Aborting !"
+	echo ""
+	exit 0
+fi
+
+if [ -f /etc/openstack-control-script-config/keystone-installed ]
+then
+	echo ""
+	echo "Keystone Proccess OK. Let's continue"
+	echo ""
+else
+	echo ""
+	echo "Keystone Proccess not completed. Aborting !"
+	echo ""
+	exit 0
+fi
+
+if [ -f /etc/openstack-control-script-config/heat-installed ]
+then
+	echo ""
+	echo "This module was already completed. Exiting !"
+	echo ""
+	exit 0
+fi
+
+#
+# We proceed to install HEAT Packages
+#
+
+echo ""
+echo "Installing HEAT Packages"
+
+yum install -y openstack-heat-api \
+	openstack-heat-api-cfn \
+	openstack-heat-common \
+	python-heatclient \
+	openstack-heat-engine \
+	openstack-utils \
+	openstack-selinux
+
+echo "Done"
+echo ""
+
+source $keystone_admin_rc_file
+
+echo ""
+echo "Configuring HEAT"
+echo ""
+
+#
+# By using python based tools, we proceed to configure heat.
+#
+
+cat /usr/share/heat/heat-dist.conf > /etc/heat/heat.conf
+cat /usr/share/heat/api-paste-dist.ini > /etc/heat/api-paste.ini
+
+chown -R heat.heat /etc/heat
+
+
+case $dbflavor in
+"mysql")
+	crudini --set /etc/heat/heat.conf database connection mysql://$heatdbuser:$heatdbpass@$dbbackendhost:$mysqldbport/$heatdbname
+	;;
+"postgres")
+	crudini --set /etc/heat/heat.conf database connection postgresql://$heatdbuser:$heatdbpass@$dbbackendhost:$psqldbport/$heatdbname
+	;;
+esac
+
+crudini --set /etc/heat/heat.conf database retry_interval 10
+crudini --set /etc/heat/heat.conf database idle_timeout 3600
+crudini --set /etc/heat/heat.conf database min_pool_size 1
+crudini --set /etc/heat/heat.conf database max_pool_size 10
+crudini --set /etc/heat/heat.conf database max_retries 100
+crudini --set /etc/heat/heat.conf database pool_timeout 10
+crudini --set /etc/heat/heat.conf database backend heat.db.sqlalchemy.api
+ 
+crudini --set /etc/heat/heat.conf DEFAULT host $heathost
+crudini --set /etc/heat/heat.conf DEFAULT debug false
+crudini --set /etc/heat/heat.conf DEFAULT verbose false
+crudini --set /etc/heat/heat.conf DEFAULT log_dir /var/log/heat
+ 
+crudini --set /etc/heat/heat.conf DEFAULT heat_metadata_server_url http://$heathost:8000
+crudini --set /etc/heat/heat.conf DEFAULT heat_waitcondition_server_url http://$heathost:8000/v1/waitcondition
+crudini --set /etc/heat/heat.conf DEFAULT heat_watch_server_url http://$heathost:8003
+crudini --set /etc/heat/heat.conf DEFAULT heat_stack_user_role $heat_stack_user_role
+# Deprecated ????
+# crudini --set /etc/heat/heat.conf DEFAULT auth_encryption_key $heatencriptionkey
+crudini --set /etc/heat/heat.conf DEFAULT use_syslog False
+ 
+crudini --set /etc/heat/heat.conf heat_api_cloudwatch bind_host 0.0.0.0
+crudini --set /etc/heat/heat.conf heat_api_cloudwatch bind_port 8003
+ 
+crudini --set /etc/heat/heat.conf heat_api bind_host 0.0.0.0
+crudini --set /etc/heat/heat.conf heat_api bind_port 8004
+ 
+crudini --set /etc/heat/heat.conf keystone_authtoken project_name $keystoneservicestenant
+crudini --set /etc/heat/heat.conf keystone_authtoken username $heatuser
+crudini --set /etc/heat/heat.conf keystone_authtoken password $heatpass
+crudini --set /etc/heat/heat.conf keystone_authtoken auth_uri http://$keystonehost:5000
+crudini --set /etc/heat/heat.conf keystone_authtoken auth_url http://$keystonehost:35357
+crudini --set /etc/heat/heat.conf keystone_authtoken project_domain_id $keystonedomain
+crudini --set /etc/heat/heat.conf keystone_authtoken user_domain_id $keystonedomain
+crudini --set /etc/heat/heat.conf keystone_authtoken signing_dir /tmp/keystone-signing-heat
+crudini --set /etc/heat/heat.conf keystone_authtoken auth_version v3
+crudini --set /etc/heat/heat.conf keystone_authtoken auth_plugin password
+crudini --set /etc/heat/heat.conf keystone_authtoken auth_section keystone_authtoken
+ 
+crudini --set /etc/heat/heat.conf trustee project_name $keystoneservicestenant
+crudini --set /etc/heat/heat.conf trustee username $heatuser
+crudini --set /etc/heat/heat.conf trustee password $heatpass
+crudini --set /etc/heat/heat.conf trustee auth_uri http://$keystonehost:5000
+crudini --set /etc/heat/heat.conf trustee auth_url http://$keystonehost:35357
+crudini --set /etc/heat/heat.conf trustee project_domain_id $keystonedomain
+crudini --set /etc/heat/heat.conf trustee user_domain_id $keystonedomain
+crudini --set /etc/heat/heat.conf trustee signing_dir /tmp/keystone-signing-heat
+crudini --set /etc/heat/heat.conf trustee auth_version v3
+ 
+crudini --set /etc/heat/heat.conf clients_keystone auth_uri http://$keystonehost:5000
+ 
+crudini --set /etc/heat/heat.conf ec2authtoken auth_uri http://$keystonehost:5000
+ 
+crudini --set /etc/heat/heat.conf DEFAULT control_exchange openstack
+
+case $brokerflavor in
+"qpid")
+	crudini --set /etc/heat/heat.conf DEFAULT rpc_backend qpid
+	crudini --set /etc/heat/heat.conf oslo_messaging_qpid qpid_hostname $messagebrokerhost
+	crudini --set /etc/heat/heat.conf oslo_messaging_qpid qpid_port 5672
+	crudini --set /etc/heat/heat.conf oslo_messaging_qpid qpid_username $brokeruser
+	crudini --set /etc/heat/heat.conf oslo_messaging_qpid qpid_password $brokerpass
+	crudini --set /etc/heat/heat.conf oslo_messaging_qpid qpid_heartbeat 60
+	crudini --set /etc/heat/heat.conf oslo_messaging_qpid qpid_protocol tcp
+	crudini --set /etc/heat/heat.conf oslo_messaging_qpid qpid_tcp_nodelay True
+	;;
+ 
+"rabbitmq")
+	crudini --set /etc/heat/heat.conf DEFAULT rpc_backend rabbit
+	crudini --set /etc/heat/heat.conf oslo_messaging_rabbit rabbit_host $messagebrokerhost
+	crudini --set /etc/heat/heat.conf oslo_messaging_rabbit rabbit_password $brokerpass
+	crudini --set /etc/heat/heat.conf oslo_messaging_rabbit rabbit_userid $brokeruser
+	crudini --set /etc/heat/heat.conf oslo_messaging_rabbit rabbit_port 5672
+	crudini --set /etc/heat/heat.conf oslo_messaging_rabbit rabbit_use_ssl false
+	crudini --set /etc/heat/heat.conf oslo_messaging_rabbit rabbit_virtual_host $brokervhost
+	crudini --set /etc/heat/heat.conf oslo_messaging_rabbit rabbit_max_retries 0
+	crudini --set /etc/heat/heat.conf oslo_messaging_rabbit rabbit_retry_interval 1
+	crudini --set /etc/heat/heat.conf oslo_messaging_rabbit rabbit_ha_queues false
+	;;
+esac
+ 
+crudini --set /etc/heat/heat.conf DEFAULT stack_domain_admin $stack_domain_admin
+crudini --set /etc/heat/heat.conf DEFAULT stack_domain_admin_password $stack_domain_admin_password
+crudini --set /etc/heat/heat.conf DEFAULT stack_user_domain_name $stack_user_domain_name
+ 
+echo ""
+echo "Heat Configured"
+echo ""
+
+# No longer needed in "L"
+# echo ""
+# echo "Create the heat domain in Identity service"
+# echo ""
+#
+# source $keystone_admin_rc_file
+# 
+# heat-keystone-setup-domain \
+# 	--stack-user-domain-name $stack_user_domain_name \
+# 	--stack-domain-admin $stack_domain_admin \
+# 	--stack-domain-admin-password $stack_domain_admin_password \
+# 	> /dev/null 2>&1
+# 
+
+
+#
+# We proceed to provision/update HEAT Database
+#
+
+echo ""
+echo "Provisioning heat DB"
+echo ""
+
+chown -R heat.heat /var/log/heat /etc/heat
+heat-manage db_sync
+chown -R heat.heat /etc/heat /var/log/heat
+
+echo ""
+echo "Done"
+echo ""
+
+#
+# We proceed to apply IPTABLES rules and start/enable Heat services
+#
+
+echo ""
+echo "Applying IPTABLES Rules"
+
+iptables -A INPUT -p tcp -m multiport --dports 8000,8004 -j ACCEPT
+service iptables save
+
+echo "Done"
+
+echo ""
+echo "Cleaning UP App logs"
+ 
+for mylog in `ls /var/log/heat/*.log`; do echo "" > $mylog;done
+ 
+echo "Done"
+echo ""
+
+echo ""
+echo "Starting HEAT"
+echo ""
+
+servicelist='
+	openstack-heat-api
+	openstack-heat-api-cfn
+	openstack-heat-engine
+'
+
+for myservice in $servicelist
+do
+	echo "Starting and Enabling Service: $myservice"
+	systemctl start $myservice
+	systemctl enable $myservice
+	systemctl status $myservice
+done
+
+# service openstack-heat-api start
+# service openstack-heat-api-cfn start
+# service openstack-heat-engine start
+# chkconfig openstack-heat-api on
+# chkconfig openstack-heat-api-cfn on
+# chkconfig openstack-heat-engine on
+
+
+#
+# Finally, we proceed to verify if HEAT was properlly installed. If not, we stop further procedings.
+#
+
+testheat=`rpm -qi openstack-heat-common|grep -ci "is not installed"`
+if [ $testheat == "1" ]
+then
+	echo ""
+	echo "HEAT Installation FAILED. Aborting !"
+	echo ""
+	exit 0
+else
+	date > /etc/openstack-control-script-config/heat-installed
+	date > /etc/openstack-control-script-config/heat
+fi
+
+
+echo ""
+echo "Heat Installed and Configured"
+echo ""
+
